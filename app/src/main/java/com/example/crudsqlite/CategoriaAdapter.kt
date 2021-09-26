94d607eef5e6a0bd7c49488c47d0bd12417bd325
keu <kevinsetup900@gmail.com>
2021-09-26 01:00:55 -0500

    crud completao

--- app/src/main/java/com/example/crudsqlite/CategoriaAdapter.kt
+++ app/src/main/java/com/example/crudsqlite/CategoriaAdapter.kt
@@ -3,17 +3,29 @@ package com.example.crudsqlite
 import android.view.LayoutInflater
 import android.view.View;
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.AdapterListUpdateCallback
 import androidx.recyclerview.widget.RecyclerView
 
 
 class CategoriaAdapter : RecyclerView.Adapter<CategoriaAdapter.CategoriaViewHolder>() {
     private var catList : ArrayList<CategoriaModel> = ArrayList();
+    private var onClickItem : ((CategoriaModel)->Unit)? = null
+    private var onClickDeleteItem : ((CategoriaModel)->Unit)? = null
 
     fun addItems(items : ArrayList<CategoriaModel>){
         this.catList = items;
         notifyDataSetChanged()
     }
+    fun setOnClickItem(callback:(CategoriaModel) -> Unit){
+        this.onClickItem = callback
+    }
+    fun setOnclickDeleteItem(callback: (CategoriaModel) -> Unit){
+        this.onClickDeleteItem = callback
+    }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =  CategoriaViewHolder (
         LayoutInflater.from(parent.context).inflate(R.layout.activity_category_item, parent, false)
@@ -22,6 +34,8 @@ class CategoriaAdapter : RecyclerView.Adapter<CategoriaAdapter.CategoriaViewHold
     override fun onBindViewHolder(holder: CategoriaViewHolder, position: Int) {
     val cat = catList[position]
         holder.bindView(cat);
+        holder.itemView.setOnClickListener{ onClickItem?.invoke(cat) }
+        holder.btnDelete.setOnClickListener{onClickDeleteItem?.invoke(cat)}
     }
 
 
@@ -32,6 +46,7 @@ class CategoriaAdapter : RecyclerView.Adapter<CategoriaAdapter.CategoriaViewHold
 
     class CategoriaViewHolder(var view : View) : RecyclerView.ViewHolder(view){
         private var name = view.findViewById<TextView>(R.id.name);
+         var btnDelete = view.findViewById<Button>(R.id.btnDelete)
 
 
 
--- app/src/main/java/com/example/crudsqlite/CreateCategoryActivity.kt
+++ app/src/main/java/com/example/crudsqlite/CreateCategoryActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,10 +15,17 @@ import androidx.recyclerview.widget.RecyclerView
 class CreateCategoryActivity : AppCompatActivity() {
     private lateinit var btnAdd: Button
     private lateinit var btnView: Button
+    private lateinit var btnUpdate : Button
     private lateinit var edName: EditText
     private lateinit var sqliteHelper: SQLiteHelper;
+
     private lateinit var recyclerView: RecyclerView
+
+
+
     private var adapter: CategoriaAdapter? = null;
+    private var cat:CategoriaModel? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +34,24 @@ class CreateCategoryActivity : AppCompatActivity() {
         initRecyclerView();
 
         sqliteHelper = SQLiteHelper(this);
-
+        btnUpdate= findViewById(R.id.btnUpdate)!!
         edName = findViewById(R.id.til_create_category_name)
         btnAdd = findViewById(R.id.btn_button)
         btnView = findViewById(R.id.btn_button_view)
 
+
         btnAdd.setOnClickListener { addCategoria() }
         btnView.setOnClickListener { getCategoria() }
+        btnUpdate.setOnClickListener {updateCategoria()}
+        adapter?.setOnClickItem {
+            Toast.makeText(this, it.name , Toast.LENGTH_SHORT).show()
+            edName.setText(it.name);
+            cat = it;
+
+        }
+        adapter?.setOnclickDeleteItem {
+            deleteCategoria(it.id)
+        }
 
 
     }
@@ -41,6 +60,25 @@ class CreateCategoryActivity : AppCompatActivity() {
         val catList = sqliteHelper.getAllCategorias();
         Log.e("ppp", "${catList.size}")
         adapter?.addItems(catList)
+
+
+    }
+    private fun updateCategoria(){
+        val name = edName.text.toString();
+        if(name === cat?.name){
+            Toast.makeText(this, "Registro no actualizado", Toast.LENGTH_LONG).show();
+            return
+        }
+        if(cat == null) return
+            val cat = CategoriaModel(id = cat!!.id, name = name)
+            val status = sqliteHelper.updateCategoria(cat)
+        if(status > -1){
+            clearEditText();
+            getCategoria()
+
+        }else{
+            Toast.makeText(this, "Actualizado Correctamente", Toast.LENGTH_LONG).show();
+        }
 
 
     }
@@ -54,6 +92,7 @@ class CreateCategoryActivity : AppCompatActivity() {
             val cat = CategoriaModel(name = name)
             val status = sqliteHelper.insertCategoria(cat);
             if (status > -1) {
+
                 Toast.makeText(this, "Categoria añadida", Toast.LENGTH_SHORT).show()
                 clearEditText()
             } else {
@@ -63,6 +102,22 @@ class CreateCategoryActivity : AppCompatActivity() {
 
 
     }
+    private fun deleteCategoria(id :Int ){
+        if(id == null) return
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Estás seguro de elimianr este elemento?")
+        builder.setCancelable(true);
+        builder.setPositiveButton("si" ){ dialog, _->
+            sqliteHelper.deleteCategoria(id);
+            getCategoria()
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("no"){ dialog, _->
+            dialog.dismiss()
+        }
+        val alert = builder.create()
+        alert.show()
+        }
 
     private fun initRecyclerView() {
         recyclerView = findViewById(R.id.recyclerView);
